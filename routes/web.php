06465c6a812635d14e574e5627074991aa856312
ゆうきゃん <ucan.lab.ggl@gmail.com>
2019-05-24 19:44:40 +0900

    feat create task validate

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,17 @@ Route::get('/', function () {
 });
 
 Route::post('/task', function (Request $request) {
- //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // TODO: Add New Task
 });
 
 Route::delete('/task/{task}', function (Task $task) {
